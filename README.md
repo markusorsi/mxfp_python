0530f10e8736f9c18168482c328bd5b775a834f7
Markus Orsi <markusorsi@icloud.com>
2024-09-27 11:18:40 +0200

    Updated error handling

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # **MXFP** (**M**acromolecule e**X**tended **F**inger**P**rint)
 
-<img src="https://img.shields.io/pypi/v/mxfp?color=success&label=Version&style=flat-square"/> <img src="https://img.shields.io/badge/Python-3.6-blue?style=flat-square"/> <img src="https://img.shields.io/badge/License-MIT-yellow?style=flat-square"/>
+<img src="https://img.shields.io/pypi/v/mxfp?color=success&label=Version&style=flat-square"/> <img src="https://img.shields.io/badge/Python-3.11-blue?style=flat-square"/> <img src="https://img.shields.io/badge/License-MIT-yellow?style=flat-square"/>
 
 ## Theory
 
-MXFP (Macromolecule eXtenden FingerPrint) is a 217D fuzzy fingerprint that encodes atom pairs of 7 pharmacophore groups and is suitable for the comparison of large molecules and scaffold hopping. In a first step, every atom is assigned to one or more of following pharmacophore categories:
+MXFP (Macromolecule eXtended FingerPrint) is a 217-dimensional fuzzy fingerprint that encodes atom pairs of seven pharmacophore groups, making it suitable for the comparison of large molecules and scaffold hopping. Each atom is assigned to one or more of the following pharmacophore categories:
 
 1. Heavy Atoms (HAC)
 2. Hydrophobic Atoms (HYD)
@@ -14,101 +14,96 @@ MXFP (Macromolecule eXtenden FingerPrint) is a 217D fuzzy fingerprint that encod
 6. Positively Charged Atoms (POS)
 7. Negatively Charged Atoms (NEG)
 
-For each pharmacophore category, all possible atom pairs are determined and converted to a Gaussian of 18% width centered at the atom pair topological (2D) or Euclidean (3D) distance. This Gaussian is then sampled at 31 distance bins $(d_{i})$ spanning from $d_{0} = 0$ to $d_{30} = 317.8$ bonds at exponentially increasing intervals. The Gaussian value $g_{jk}(d_{i})$ of atom pair with distance $d_{jk}$ between two atoms for distance bin $d_{i}$ is calculated as follows:
+For each pharmacophore category, all possible atom pairs are determined and converted to a Gaussian with an 18% width centered at the atom pair topological (2D) or Euclidean (3D) distance. This Gaussian is sampled at 31 distance bins \((d_{i})\) spanning from \(d_{0} = 0\) to \(d_{30} = 317.8\) bonds at exponentially increasing intervals. The Gaussian value \(g_{jk}(d_{i})\) for an atom pair with distance \(d_{jk}\) is calculated as follows:
 
-$$ g_{jk}(d_{i}) = e^{- \frac{1}{2}(\frac{d_{i} - d_{jk}}{d_{jk}*0.09})^2} $$
+$$ g_{jk}(d_{i}) = e^{- \frac{1}{2} \left( \frac{d_{i} - d_{jk}}{d_{jk} \cdot 0.09} \right)^2 } $$
 
-Each of the obtained 31 gaussian values is normalized to the sum of all 31 values, $s_{jk}$, so that every atom pair contributes equally to the fingerprint.
+Each of the obtained 31 Gaussian values is normalized to the sum of all 31 values, \(s_{jk}\), to ensure that every atom pair contributes equally to the fingerprint.
 
 $$ s_{jk}  = \sum_{i = 0}^{30} g_{jk}(d_{i}) $$
 
-The sum of normalized gaussian contributions from all atom pairs of one pharmacophore category at distance $d_{i}$ is normalized by the total number of atoms in that category $N_{c}$ to the power 1.5 to reduce the sensitivity of the fingerprint to molecule size, multiplied by 100 and rounded to unity to give the final fingerprint bit value $v_{ci}$.
+The sum of normalized Gaussian contributions from all atom pairs of one pharmacophore category at distance \(d_{i}\) is normalized by the total number of atoms in that category \(N_{c}\) raised to the power of 1.5 to reduce the sensitivity of the fingerprint to molecule size. This value is multiplied by 100 and rounded to yield the final fingerprint bit value \(v_{ci}\).
 
-$$ v_{ci} = \frac{100}{N_{c}^{1.5}} \sum_{j = 1} \sum_{k = 1} \frac{g_{jk}(d_{i})}{s_{jk}}$$ 
+$$ v_{ci} = \frac{100}{N_{c}^{1.5}} \sum_{j = 1} \sum_{k = 1} \frac{g_{jk}(d_{i})}{s_{jk}} $$
 
-The 31 fingerprint bit values from each of the 7 atom categories are finally joined, yielding the 217D fingerprint vector.
+The 31 fingerprint bit values from each of the 7 atom categories are concatenated, yielding the 217-dimensional fingerprint vector.
 
-## Getting started
+## Getting Started
 
 ### Prerequisites
 
-You will need following prerequisites: 
+You will need the following prerequisites: 
 
-* [Python 3.6](https://www.python.org)
-* [NumPy](https://numpy.org)
-* [RDKit](https://www.rdkit.org)
+- [Python 3.6](https://www.python.org)
+- [NumPy](https://numpy.org)
+- [RDKit](https://www.rdkit.org)
 
 ## Installing MXFP
 
-There are several ways in which you can get started using MXFP:
+There are several ways to get started using MXFP:
 
 #### **Installing via GitHub**
 
-To obtain a local copy of the project clone the GitHub repository as follows:
+To obtain a local copy of the project, clone the GitHub repository:
 
-```console
+```bash
 git clone https://github.com/reymond-group/mxfp_python.git
 ```
 
 #### **Installing via Conda**
 
-To create a ready to use Conda environment download the mxfp.yml file from the repository and run following commands:
+To create a ready-to-use Conda environment, download the `mxfp.yml` file from the repository and run the following commands:
 
-```console
+```bash
 conda env create -f mxfp.yml
-```
-
-```console
 conda activate mxfp
 ```
 
 #### **Installing via pip**
 
-To install mxfp on an existing Conda environment activate the environment and install mxfp via pip using following commands:
+To install MXFP in an existing Conda environment, activate the environment and install MXFP via pip:
 
-```console
+```bash
 conda activate my_environment
-```
-
-```console
 pip install mxfp
 ```
 
 ## Using MXFP
 
-In your Python script or Jupyter cell:
+In your Python script or Jupyter notebook:
 
-1. Import the required libraries (RDKit, MXFP)
-2. Convert SMILES to rdchem.Mol object with RDKit (optional)
-3. Initialize the MXFPCalculator class
-4. Calculate MXFP of your molecule either from rchem.Mol object or directly from SMILES
+1. Import the required libraries (RDKit, MXFP).
+2. Convert SMILES to an `rdchem.Mol` object with RDKit (optional).
+3. Initialize the `MXFPCalculator` class.
+4. Calculate the MXFP of your molecule either from the `rdchem.Mol` object or directly from SMILES.
 
 ```python
-#Import the required libraries (RDKit, MXFP)
+# Import the required libraries (RDKit, MXFP)
 from rdkit import Chem
-from mxfp import mxfp
+from mxfp import MXFPCalculator
 
-#Convert SMILES to rdchem.Mol object with RDKit (optional)
+# Convert SMILES to rdchem.Mol object with RDKit (optional)
 polymyxin_b2_smiles = 'C[C@H]([C@H]1C(=O)NCC[C@@H](C(=O)N[C@H](C(=O)N[C@@H](C(=O)N[C@H](C(=O)N[C@H](C(=O)N[C@H](C(=O)N1)CCN)CCN)CC(C)C)CC2=CC=CC=C2)CCN)NC(=O)[C@H](CCN)NC(=O)[C@H]([C@@H](C)O)NC(=O)[C@H](CCN)NC(=O)CCCCC(C)C)O'
 polymyxin_b2_mol = Chem.MolFromSmiles(polymyxin_b2_smiles)
 
-#Initialize the MXFPCalculator class
-MXFP = mxfp.MXFPCalculator()
+# Initialize the MXFPCalculator class
+calculator = MXFPCalculator()
 
-#Calculate MXFP of your molecule either from rchem.Mol object or directly from SMILES
-polymyxin_b2_mxfp = MXFP.mxfp_from_mol(polymyxin_b2_mol) #from rdchem.Mol object
-polymyxin_b2_mxfp = MXFP.mxfp_from_smiles(polymyxin_b2_smiles) #from SMILES
+# Calculate MXFP of your molecule from rdchem.Mol object or directly from SMILES
+polymyxin_b2_mxfp = calculator.mxfp_from_mol(polymyxin_b2_mol)  # from rdchem.Mol object
+polymyxin_b2_mxfp = calculator.mxfp_from_smiles(polymyxin_b2_smiles)  # from SMILES
 ```
 
-If you are working with 3D coordinates and wish to use Euclidean atom-pair distances instead of topological distances, initialize the MXFPCalculator class using the dimensionality='3D' parameter. Mind that you will not be able to calculate MXFP from a SMILES string if you use the '3D' option, so you need to provide an rdchem.Mol object.
+If you are working with 3D coordinates and wish to use Euclidean atom-pair distances instead of topological distances, initialize the `MXFPCalculator` class with the `dimensionality='3D'` parameter. Note that you will not be able to calculate MXFP from a SMILES string if you use the '3D' option, so you need to provide an `rdchem.Mol` object.
 
 ```python
-#Initialize the MXFPCalculator class
-MXFP = mxfp.MXFPCalculator(dimensionality='3D')
+# Initialize the MXFPCalculator class for 3D calculations
+calculator_3d = MXFPCalculator(dimensionality='3D')
 ```
 
 ## License
-[MIT](https://choosealicense.com/licenses/mit/)
+
+This project is licensed under the [MIT License](https://choosealicense.com/licenses/mit/).
 
 ## Contact
 
--- mxfp/mxfp.py
+++ mxfp/mxfp.py
@@ -6,22 +6,22 @@ from rdkit.Chem import AllChem, rdmolops, rdchem
 
 
 DISTANCE_BINS = [0, 1, 2, 3, 4, 5, 6, 7.1, 8.4, 9.9, 11.6, 13.7, 16.2,
-             19.1, 22.6, 26.6, 31.4, 37.1, 43.7, 51.6, 60.9, 71.8, 84.8,
-             100.0, 118, 139.3, 164.4, 193.9, 228.9, 270, 318.7]
+                 19.1, 22.6, 26.6, 31.4, 37.1, 43.7, 51.6, 60.9, 71.8, 84.8,
+                 100.0, 118, 139.3, 164.4, 193.9, 228.9, 270, 318.7]
 
 MXFP_SMARTS = {
-'HAC' : ['[*]'],
-'HYD' : ['[C]', '[a]', '[S]'],
-'ARO' : ['[a]'],
-'HBA' : ['[!$([#6,F,Cl,Br,I,o,s,nX3,#7v5,#15v5,#16v4,#16v6,*+1,*+2,*+3])]'],
-'HBD' : ['[!$([#6,H0,-,-2,-3])]'],
-'POS' : ['[*+]'],
-'NEG' : ['[*-]'],
+    'HAC': ['[*]'],
+    'HYD': ['[C]', '[a]', '[S]'],
+    'ARO': ['[a]'],
+    'HBA': ['[!$([#6,F,Cl,Br,I,o,s,nX3,#7v5,#15v5,#16v4,#16v6,*+1,*+2,*+3])]'],
+    'HBD': ['[!$([#6,H0,-,-2,-3])]'],
+    'POS': ['[*+]'],
+    'NEG': ['[*-]'],
 }
 
 class MXFPCalculator:
 
-    def __init__(self, dimensionality:str='2D', max_dist:int=1000, categories:list=['HAC', 'HYD', 'ARO', 'HBA', 'HBD', 'POS', 'NEG']) -> None:
+    def __init__(self, dimensionality: str = '2D', max_dist: int = 1000, categories: list = ['HAC', 'HYD', 'ARO', 'HBA', 'HBD', 'POS', 'NEG']) -> None:
         """
         MXFP calculator class
 
@@ -32,27 +32,23 @@ class MXFPCalculator:
         
         max_dist: 1000
             Maximum atom pair distance found in the molecule. Lower values speed up calculations.
-
         """
-        self.distance_bins = DISTANCE_BINS 
+        self.distance_bins = DISTANCE_BINS
         self.mxfp_smarts = MXFP_SMARTS
         self.dimensionality = dimensionality
         self.max_dist = max_dist
         self.labels = categories
-    
-        gaussrows = []
+
+        self.gaussrows = []
         for distance in range(self.max_dist):
             if distance == 0:
-                gaussrow = [exp(-0.5*(((distance_bin)/(0.09))**2)) for distance_bin in DISTANCE_BINS]
+                gaussrow = [exp(-0.5 * (((distance_bin) / (0.09)) ** 2)) for distance_bin in DISTANCE_BINS]
             else:
-                gaussrow = [exp(-0.5*(((distance_bin-distance)/(distance*0.09))**2)) for distance_bin in DISTANCE_BINS]
-            gaussrows.append(gaussrow)
-        
-        self.gaussrows = gaussrows
+                gaussrow = [exp(-0.5 * (((distance_bin - distance) / (distance * 0.09)) ** 2)) for distance_bin in DISTANCE_BINS]
+            self.gaussrows.append(gaussrow)
 
-    def get_property_matrix(self, mol:rdchem.Mol) -> np.ndarray:
+    def get_property_matrix(self, mol: rdchem.Mol) -> np.ndarray:
         """ 
-
         Parameters 
         ----------
         mol : rdkit.Chem.rdchem.Mol
@@ -64,32 +60,33 @@ class MXFPCalculator:
             (7 pharmacophore categories) x (number of heavy atoms) matrix.\n 
             Each heavy atom is represented by a column of 7 binary values,\n
             which state whether the selected atom possesses the property (1) or does not possess the property (0).
-
         """
-
         propmat = np.zeros((len(self.labels), mol.GetNumAtoms()))
 
-        try: 
-            for i, label in enumerate(self.labels):
+        for i, label in enumerate(self.labels):
+            try:
                 category = self.mxfp_smarts[label]
+                matched = False
                 for smarts in category:
                     pattern = Chem.MolFromSmarts(smarts)
-                    matched = False
+                    if pattern is None:
+                        raise ValueError(f"Invalid SMARTS pattern: {smarts}")
+                    
                     for match in mol.GetSubstructMatches(pattern, uniquify=True):
                         for j in match:
                             propmat[i, j] = 1
                         matched = True
                     if matched: 
                         break
-            return propmat
-        except:
-            print("Oopsie! Something went wrong while trying to generate the property matrix.\n"
-                "Have you checked if the input file format and/or the input molecule is valid?")
+            except ValueError as e:
+                print(f"Error processing label '{label}': {e}")
+            except Exception as e:
+                print(f"Error while generating property matrix: {e}")
+        
+        return propmat
 
-
-    def yield_ap(self, mol:rdchem.Mol, property:int):
+    def yield_ap(self, mol: rdchem.Mol, property: int):
         """
-
         Parameters 
         ----------
         mol : rdkit.Chem.rdchem.Mol
@@ -109,9 +106,7 @@ class MXFPCalculator:
         -------
          atom pairs : Generator
             Returns a generator object containing all pairs of atoms displaying the selected pharmacophore property.
-
         """
-
         propmat = self.get_property_matrix(mol)
 
         if self.dimensionality == '3D':
@@ -120,17 +115,15 @@ class MXFPCalculator:
             distmat = rdmolops.GetDistanceMatrix(mol)
 
         try:
-            for i in range(0, mol.GetNumAtoms()):
+            for i in range(mol.GetNumAtoms()):
                 for j in range(i, mol.GetNumAtoms()):
-                    if propmat[property, i] and propmat[property, j] == 1:
+                    if propmat[property, i] == 1 and propmat[property, j] == 1:
                         yield int(distmat[i, j])
-        except:
-            print(f"Uh-Oh! Couldn't calculate the atom pairs for property {property}")
+        except Exception as e:
+            print(f"Error calculating atom pairs for property {property}: {e}")
 
-
-    def get_gausslist(self, mol:rdchem.Mol, property:int) -> tuple:
+    def get_gausslist(self, mol: rdchem.Mol, property: int) -> tuple:
         """
-
         Parameters 
         ----------
         mol : rdkit.Chem.rdchem.Mol
@@ -154,20 +147,16 @@ class MXFPCalculator:
 
         gaussum : int
             List containing the sum of the 31 Gaussian values for each atom pair. 
-
         """
-
         try: 
             gausslist = np.array([self.gaussrows[distance] for distance in self.yield_ap(mol, property)])
             gausssum = np.sum(gausslist.T, axis=0)
             return gausslist, gausssum
-        except:
-            print(f"Yikes! Couldn't generate the list and the sum of the Gaussian values for property {property}")
+        except Exception as e:
+            print(f"Error generating Gaussian values for property {property}: {e}")
 
-
-    def count_atoms(self, mol:rdchem.Mol, property:int) -> int:
+    def count_atoms(self, mol: rdchem.Mol, property: int) -> int:
         """
-
         Parameters 
         ----------
         mol : rdkit.Chem.rdchem.Mol
@@ -187,21 +176,17 @@ class MXFPCalculator:
         -------
         num_atoms : int 
             Count of all atoms that possess the selected pharmacophore property.
-
         """
-
         propmat = self.get_property_matrix(mol)
 
         try:
             num_atoms = np.count_nonzero(propmat[property] == 1)
             return num_atoms
-        except:
-            print(f"Ooof! Couldn't count the atoms with property {property}")
+        except Exception as e:
+            print(f"Error counting atoms with property {property}: {e}")
 
-
-    def calc_property_mxfp(self, mol:rdchem.Mol, property:int) -> np.ndarray:
+    def calc_property_mxfp(self, mol: rdchem.Mol, property: int) -> np.ndarray:
         """
-
         Parameters 
         ----------
         mol : rdkit.Chem.rdchem.Mol
@@ -221,24 +206,21 @@ class MXFPCalculator:
         -------
         property_mxfp : np.ndarray
             MXFP values for the selected property (31 values)
-
         """
-        
-        gausslist, gausssum = self.get_gausslist(mol, property)
-        natoms = self.count_atoms(mol, property)
-
-        property_mxfp = np.zeros(len(self.distance_bins))
-
         try:
-            for i in range(len(self.distance_bins)):
-                if natoms != 0:
-                    property_mxfp[i] = np.floor((100/(natoms**1.5))*np.sum(gausslist[:, i]/gausssum))
-            return property_mxfp
-        except:
-            print(f"Yuck! Something didn't work as expected. Couldn't generate MXFP values for property {property}")
-        
+            gausslist, gausssum = self.get_gausslist(mol, property)
+            natoms = self.count_atoms(mol, property)
 
-    def mxfp_from_mol(self, mol:rdchem.Mol) -> np.ndarray:
+            property_mxfp = np.zeros(len(self.distance_bins))
+
+            if natoms != 0:
+                for i in range(len(self.distance_bins)):
+                    property_mxfp[i] = np.floor((100 / (natoms ** 1.5)) * np.sum(gausslist[:, i] / gausssum))
+            return property_mxfp
+        except Exception as e:
+            print(f"Error generating MXFP values for property {property}: {e}")
+
+    def mxfp_from_mol(self, mol: rdchem.Mol) -> np.ndarray:
         """
         Calculates the MXFP for a selected molecule. 
 
@@ -252,34 +234,45 @@ class MXFPCalculator:
         mxfp : np.ndarray
             MXFP for the selected molecule (217 values).
         """
-
         try:
             mxfp = np.concatenate([self.calc_property_mxfp(mol, i) for i in range(len(self.labels))], axis=0)
-            mxfp = mxfp.astype(int)
-            return mxfp
-        except:
-            print("Ouch! Couldn't calculate MXFP for this molecule.")
+            return mxfp.astype(int)
+        except Exception as e:
+            print(f"Error calculating MXFP for this molecule: {e}")
 
+    def mxfp_from_smiles(self, smiles: str) -> np.ndarray:
+        """
+        Calculates the MXFP for a selected molecule. 
 
-    def mxfp_from_smiles(self, smiles:str) -> np.ndarray:
-            """
-            Calculates the MXFP for a selected molecule. 
+        Parameters 
+        ----------
+        smiles : str
+            A SMILES string of a molecule.
 
-            Parameters 
-            ----------
-            smiles : str
-                A SMILES string of .
+        Returns
+        -------
+        mxfp : np.ndarray
+            MXFP for the selected molecule (217 values).
+        
+        Example Usage
+        -------------
+        ```python
+        from mxfp.mxfp import MXFPCalculator
 
-            Returns
-            -------
-            mxfp : np.ndarray
-                MXFP for the selected molecule (217 values).
-            """
+        calculator = MXFPCalculator()
+        mxfp = calculator.mxfp_from_smiles('CC(C)CCCCC(=O)N[C@@H](CCN)C(=O)N[C@@H]([C@@H](C)O)C(=O)N[C@@H](CCN)C(=O)N[C@H]1CCNC(=O)[C@@H](NC(=O)[C@H](CCN)NC(=O)[C@H](CCN)NC(=O)[C@H](CC(C)C)NC(=O)[C@@H](Cc2ccccc2)NC(=O)[C@H](CCN)NC1=O)[C@@H](C)O')
+        
+        print(mxfp)
+        ```
 
-            try:
-                mol = Chem.MolFromSmiles(smiles)
-                mxfp = np.concatenate([self.calc_property_mxfp(mol, i) for i in range(len(self.labels))], axis=0)
-                mxfp = mxfp.astype(int)
-                return mxfp
-            except:
-                print("Ouch! Couldn't calculate MXFP for this molecule. Have you checked if the SMILES string is valid?")
+        """
+        try:
+            mol = Chem.MolFromSmiles(smiles)
+            if mol is None:
+                raise ValueError(f"Invalid SMILES string: {smiles}")
+            
+            return self.mxfp_from_mol(mol)
+        except ValueError as e:
+            print(f"Error processing SMILES string: {e}")
+        except Exception as e:
+            print(f"Error calculating MXFP from SMILES: {e}")
